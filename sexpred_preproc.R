c64f09007a4d55f067a35434368870e772266c23
Florent Chuffart <florent.chuffart@gmail.com>
2019-12-02 17:38:43 +0100

    Màj

--- sexpred_preproc.R
+++ sexpred_preproc.R
@@ -20,14 +20,15 @@ s
 gs = colnames(d)[13:1012]
 g = gs[1]
 d[[g]]
-g = "ATP10B"
+# g = "ATP10B"
 
 
 layout(matrix(1:2, 1), respect=TRUE)
 boxplot(d[[g]]~s)
 plot(d[[g]], s, main=paste0("sex~",g), xlab=g, ylab="sex")
 # P(Y=1|X) = logitinv(a + b.x)
-m = glm(s~d[[g]], family = binomial(logit))
+m = glm(c(s,0,0,1,1)~c(d[[g]],0,max(d[,gs]),0,max(d[,gs])), 
+  family = binomial(logit))
 summary(m)$coefficients
 pv = summary(m)$coefficients[2,4]
 logitinv = function(x) 1/(1 + exp(-x))
@@ -36,14 +37,6 @@ lines(x, logitinv(m$coefficients[[1]] + m$coefficients[[2]]*x), col=2, lwd=2)
 legend("bottomright", "logit(Y)=b.X", col=2, lty=1, cex=0.6)
 
 
-
-pvs = sapply(gs, function(g) {
-  print(g)
-  m = glm(s~d[[g]], family = binomial(logit))
-  b = m$coefficients[2]
-  pv = summary(m)$coefficients[2,4]
-  c(pv,b)
-})
 
 layout(matrix(1:2, 1), respect=TRUE)
 g = "SNORA8"
